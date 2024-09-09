1f0afbd30b9a583966b6b38931cefd682372bcd3
Marcus <marcusvinidias1@gmail.com>
2024-09-09 10:21:10 -0400

    animacao

---
+++ DesignerAnimacao/app/src/main/java/br/com/marcus/designeranimacao/SplashScreen.java
@@ -0,0 +1,41 @@
+package br.com.marcus.designeranimacao;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class SplashScreen extends AppCompatActivity {
+
+    // CRIAR A INSTÂNCIA HANDLER --> PERMITE A EXECUÇÃO COM DELAY
+    Handler handler = new Handler();
+
+    // CARIAR A INSTÂNCIA RUNNABLE -->  PERMITE A EXECUÇÃO APOS O DELAY
+    Runnable runnable;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        // DEFINIR O RUNNALE COM A TAREFA QUE SERÁ EXECUTADA
+        // APÓS O TIME (DELAY)
+
+        runnable = new Runnable() {
+            @Override
+            public void run() {
+                Intent intencao = new Intent(SplashScreen.this, MainActivity.class);
+                startActivity(intencao);
+                finish();
+            }
+        };
+        // DEFINIR O TEMPO DE EXECUÇÃO
+
+        handler.postDelayed(runnable, 3000);
+    }
+}
